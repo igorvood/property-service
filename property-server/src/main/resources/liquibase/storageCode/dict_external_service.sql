67c6e7c6e544a91472c33ed50212ca7f02a293c9
vood <balitsky_igor@list.ru>
2020-03-14 13:32:30 +0300

    init

---
+++ property-server/src/main/resources/liquibase/storageCode/dict_external_service.sql
@@ -0,0 +1,26 @@
+create table dict_external_service
+(
+    id        varchar2(20)  not null,
+    constraint dict_external_service_pk primary key (id)
+        using index tablespace jp_idx,
+    deleted_at      date,
+    date_create timestamp,
+    reprocessing_count number
+)
+/
+comment on table dict_external_service
+    is 'Справочник внешних сервисов @Editable@'
+/
+comment on column dict_external_service.id
+    is 'Идентификатор'
+/
+comment on column dict_external_service.deleted_at
+    is 'Признак уделения'
+/
+comment on column dict_external_service.date_create
+    is 'Дата создания'
+/
+comment on column dict_external_service.reprocessing_count
+    is 'количество попыток репроцессинга'
+/
+
